9ce313e4afdfa8e295aeedbc1b69006e143f4f2c
DK99 <kaleb.dk@gmail.com>
2021-03-27 00:15:59 +0100

    move build.bat

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ rm -rf $HW_PATH/dist
 mkdir $HW_PATH/dist
 cd $HW_PATH/build
 cmake -DCMAKE_BUILD_TYPE="Release" -DNOSERVER=1 -DNOVIDEOREC=1 -DNOPNG=1 -DLUA_SYSTEM=0 -OFFICIAL_SERVER=0 -DDATA_INSTALL_DIR=$HW_PATH/dist ..
-make DESTDIR=$HW_PATH/dist install
+make DESTDIR=$HW_PATH/dist install -j4
 cd $HW_PATH/dist
 mv `find . -name "Data"` .
 mv usr/local/bin/* .
