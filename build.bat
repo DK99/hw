92c35dc739403e20e5a2b3ef6b52c331439c5ab4
DK99 <kaleb.dk@gmail.com>
2021-03-27 01:29:57 +0100

    [~] windows bats are weird?

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ call F:\env.bat
 
 set
 
-cmake -DCMAKE_TOOLCHAIN_FILE="F:\vcpkg\scripts\buildsystems\vcpkg.cmake" -G"NMake Makefiles" -DWIN32_WIN64_CROSS_COMPILE=1 -DNOSERVER=1 -DCMAKE_INSTALL_PREFIX=F:\dist -DCMAKE_BUILD_TYPE="Release" -DSDL2_BUILDING_LIBRARY=1
+cmake -G "Visual Studio 16 2019" -DCMAKE_TOOLCHAIN_FILE="F:\vcpkg\scripts\buildsystems\vcpkg.cmake" -G"NMake Makefiles" -DWIN32_WIN64_CROSS_COMPILE=1 -DNOSERVER=1 -DCMAKE_INSTALL_PREFIX=F:\dist -DCMAKE_BUILD_TYPE="Release" -DSDL2_BUILDING_LIBRARY=1
 nmake install
 copy F:\vcpkg\installed\x64-windows\bin\ogg.dll F:\dist
 copy F:\vcpkg\installed\x64-windows\bin\vorbis.dll F:\dist
